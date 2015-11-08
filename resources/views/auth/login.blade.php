88c12533adc131c5c13bc29ff407ad2776fe5d5e
Stijn van der Vegt <stijn@draad.nu>
2015-11-08 21:48:57 +0100

    setup login and register form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,29 +1,43 @@
-<html>
+@extends('layout.default')
 
-<head>
+@section('content')
 
-</head>
-<body>
-    <form method="POST" action="/auth/login">
-        {!! csrf_field() !!}
 
-        <div>
-            Email
-            <input type="email" name="email" value="{{ old('email') }}">
+    <div class="container">
+        <div class="col-md-3 col-md-offset-4">
+            <div class="panel member_signin">
+                <div class="panel-body">
+                    <div class="fa_user">
+                        <i class="fa fa-user"></i>
+                    </div>
+                    <p class="member">Member Login</p>
+
+                    <form method="POST" action="/auth/login" class="form">
+                        {!! csrf_field() !!}
+                        <div class="form-group">
+                            <label for="email">Email address</label>
+
+                            <input type="email" name="email" class="form-control" value="{{ old('email') }}">
+
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+
+                            <input type="password" class="form-control" name="password" id="password">
+
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox" name="remember"> Remember Me
+                            </label>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary btn-lg btn-block">Login</button>
+                    </form>
+                    @include('parts.errors')
+                    <a href="{{ route('register') }}">No member yet? Register here</a>
+                </div>
+            </div>
         </div>
-
-        <div>
-            Password
-            <input type="password" name="password" id="password">
-        </div>
-
-        <div>
-            <input type="checkbox" name="remember"> Remember Me
-        </div>
-
-        <div>
-            <button type="submit">Login</button>
-        </div>
-    </form>
-</body>
-</html>
+    </div>
+@stop
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,35 +1,57 @@
-<html>
+@extends('layout.default')
 
-<head>
+@section('content')
 
-</head>
-<body>
-    <form method="POST" action="/auth/register">
-        {!! csrf_field() !!}
 
-        <div>
-            Name
-            <input type="text" name="name" value="{{ old('name') }}">
+    <div class="container">
+        <div class="col-md-6 col-md-offset-4">
+            <div class="panel member_signin">
+                <div class="panel-body">
+                    <div class="fa_user">
+                        <i class="fa fa-user"></i>
+                    </div>
+                    <p class="member">Member Register</p>
+
+                    <form method="POST" action="/auth/register">
+                        {!! csrf_field() !!}
+
+                        <div class="form-group">
+                            <label for="name">Name</label>
+
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Email</label>
+
+                            <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+
+                            <input type="password" class="form-control" name="password" value="{{ old('password') }}">
+
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password_confirmation">Confirm Password</label>
+
+
+                            <input type="password" class="form-control" name="password_confirmation"
+                                   value="{{ old('password_confirmation') }}">
+
+                        </div>
+
+                        <div>
+                            <button type="submit" class="btn btn-primary btn-lg btn-block">Register</button>
+                        </div>
+                    </form>
+                    @include('parts.errors')
+                </div>
+            </div>
         </div>
-
-        <div>
-            Email
-            <input type="email" name="email" value="{{ old('email') }}">
-        </div>
-
-        <div>
-            Password
-            <input type="password" name="password">
-        </div>
-
-        <div>
-            Confirm Password
-            <input type="password" name="password_confirmation">
-        </div>
-
-        <div>
-            <button type="submit">Register</button>
-        </div>
-    </form>
-</body>
-</html>
+    </div>
+    </body>
+    </html>
